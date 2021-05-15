820a16b9dfd3bd13c72585f5cc0f3f8e3416fa42
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-15 15:45:29 +0100

    Constructors

--- Computer.cs
+++ Computer.cs
@@ -2,6 +2,11 @@ namespace OOPExamples
 {
     public class Computer
     {
-        public string Name { get; set; } = "Unknown";
+        public string Name { get; private set; }
+
+        public Computer(string name)
+        {
+            Name = name;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,14 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        var computer = new Computer();
-        Console.WriteLine($"Computer Type => {computer.GetType()}");
-
+        var computer = new Computer("Generic Computer");
         var isComputer = computer is Computer;
-        Console.WriteLine($"Is Computer => {isComputer}");
 
-        computer.Name = "Generic Computer";
+        Console.WriteLine($"Computer Type => {computer.GetType()}");
         Console.WriteLine($"Computer Name => {computer.Name}");
+        Console.WriteLine($"Is Computer => {isComputer}");
 
         Console.Read();
     }
