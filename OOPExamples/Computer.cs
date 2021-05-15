f565e3dc71910af4d2ac15fd33cc78a97c7a585f
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-15 14:26:52 +0100

    Class basics.

---
+++ OOPExamples/Computer.cs
@@ -0,0 +1,4 @@
+namespace OOPExamples
+{
+    public class Computer {}
+}
--- OOPExamples/Program.cs
+++ OOPExamples/Program.cs
@@ -5,5 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        var computer = new Computer();
+        Console.WriteLine($"Computer Type => {computer.GetType()}");
+
+        var isComputer = computer is Computer;
+        Console.WriteLine($"Is Computer => {isComputer}");
+
+        Console.Read();
     }
 }
